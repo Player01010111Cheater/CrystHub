83d356c997c420e59cba82eacec8fc6cc3fa5294
Easy lua <staskuzmin1406@gmail.com>
2025-06-27 23:31:15 +0300

    Update optimization.lua

--- optimization.lua
+++ optimization.lua
@@ -2,33 +2,43 @@ local function optimization_farm()
     local farm_path = workspace.Farm
 
     for _, v in pairs(farm_path:GetChildren()) do
+        local modelCount = 0
+
+        -- Считаем модели во всех Plants_Physical
         for _, i in pairs(v.Important.Plants_Physical:GetChildren()) do
-            -- Считаем количество моделей
-            print("Чистим базу: " .. i.Parent.Parent.Data.Owner.Value)
-            local modelCount = 0
             for _, child in pairs(i:GetChildren()) do
                 if child:IsA("Model") then
                     modelCount = modelCount + 1
                 end
             end
-            print("Количество моделей: ".. modelCount)
+        end
 
-            -- Если моделей больше 151 и база чужая
-            if modelCount > 151 and i.Parent.Parent.Data.Owner.Value ~= game.Players.LocalPlayer.Name then
-                print("Чистим базу: " .. i.Parent.Parent.Data.Owner.Value)
+        print("База: " .. v.Data.Owner.Value .. " | Моделей: " .. modelCount)
 
-                -- Удаляем объекты начиная с 151-го
-                for t = 151, #i do
-                    i[t]:Destroy()
+        -- Если моделей больше 151 и база чужая
+        if modelCount > 151 and v.Data.Owner.Value ~= game.Players.LocalPlayer.Name then
+            print("Чистим базу: " .. v.Data.Owner.Value)
+
+            -- Удаляем лишние объекты в Plants_Physical
+            for _, i in pairs(v.Important.Plants_Physical:GetChildren()) do
+                local children = i:GetChildren()
+                for t = 151, #children do
+                    children[t]:Destroy()
                 end
-
-                print("База почищена.")
             end
+
+            print("База почищена.")
         end
     end
 end
 
 local function optimization_main()
+    local notifcation_path = game.Players.LocalPlayer.PlayerGui.Top_Notification.Frame
+    optimization_farm()
+end
+
+optimization_main()
+
     local notifcation_path = game.Players.LocalPlayer.PlayerGui.Top_Notification.Frame
     optimization_farm()
 end
