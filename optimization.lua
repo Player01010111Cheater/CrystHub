8aca131681ae4c30a8a80433f22667bbd2206245
Easy lua <staskuzmin1406@gmail.com>
2025-06-27 23:25:13 +0300

    Update optimization.lua

--- optimization.lua
+++ optimization.lua
@@ -4,12 +4,14 @@ local function optimization_farm()
     for _, v in pairs(farm_path:GetChildren()) do
         for _, i in pairs(v.Important.Plants_Physical:GetChildren()) do
             -- Считаем количество моделей
+            print("Чистим базу: " .. i.Parent.Parent.Data.Owner.Value)
             local modelCount = 0
             for _, child in pairs(i:GetChildren()) do
                 if child:IsA("Model") then
                     modelCount = modelCount + 1
                 end
             end
+            print("Количество моделей: ".. modelCount)
 
             -- Если моделей больше 151 и база чужая
             if modelCount > 151 and i.Parent.Parent.Data.Owner.Value ~= game.Players.LocalPlayer.Name then
