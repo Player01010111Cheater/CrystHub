3387db9b7223c47484b2dda8e6494bfc1bc6e0eb
Easy lua <staskuzmin1406@gmail.com>
2025-07-10 18:13:10 +0300

    Create CrystalHub_GaG-petlist.lua

---
+++ CrystalHub_GaG-petlist.lua
@@ -0,0 +1,28 @@
+local pets_rarity = {
+  Common = {
+    "Bunny", "Dog", "Golden Lab", "Bee"
+  },
+  Uncommon = {
+    "Black Bunny", "Chicken", "Cat", "Deer"
+  },
+  Rare = {
+    "Orange Tabby", "Spotted Deer", "Pig", "Rooster",
+    "Monkey", "Hedgehog", "Flamingo", "Sea Turtle",
+    "Toucan", "Wasp", "Honey Bee"
+  },
+  Legendary = {
+    "Cow", "Silver Monkey", "Tarantula Hawk", "Mole",
+    "Seal", "Orangutan", "Peacock", "Ostrich",
+    "Axolotl", "Triceratops", "Stegosaurus", "Scarlet Macaw",
+    "Polar Bear", "Sea Otter", "Turtle"
+  },
+  Mythical = {
+    "Snail", "Giant Ant", "Caterpillar", "Praying Mantis",
+    "Butterfly", "Echo Frog", "Frog", "Moth",
+    "Hyacinth Macaw", "Petal Bee", "Bear Bee"
+  },
+  Divine = {
+    "Dragonfly", "Queen Bee", "Raccoon", "Night Owl",
+    "Fennec Fox", "Disco Bee", "Mimic Octopus", "T‑Rex"
+  }
+}
