8a31515af12079ca2e67d89494ffccb36850f7ef
Easy lua <staskuzmin1406@gmail.com>
2025-07-10 18:17:48 +0300

    Update CrystalHub_GaG-petlist.lua

--- CrystalHub_GaG-petlist.lua
+++ CrystalHub_GaG-petlist.lua
@@ -1,28 +1,68 @@
 local pets_rarity = {
-  Common = {
-    "Bunny", "Dog", "Golden Lab", "Bee"
+  ["Common"] = {
+    ["Bunny"] = true,
+    ["Dog"] = true,
+    ["Golden Lab"] = true,
+    ["Bee"] = true
   },
-  Uncommon = {
-    "Black Bunny", "Chicken", "Cat", "Deer"
+  ["Uncommon"] = {
+    ["Black Bunny"] = true,
+    ["Chicken"] = true,
+    ["Cat"] = true,
+    ["Deer"] = true
   },
-  Rare = {
-    "Orange Tabby", "Spotted Deer", "Pig", "Rooster",
-    "Monkey", "Hedgehog", "Flamingo", "Sea Turtle",
-    "Toucan", "Wasp", "Honey Bee"
+  ["Rare"] = {
+    ["Orange Tabby"] = true,
+    ["Spotted Deer"] = true,
+    ["Pig"] = true,
+    ["Rooster"] = true,
+    ["Monkey"] = true,
+    ["Hedgehog"] = true,
+    ["Flamingo"] = true,
+    ["Sea Turtle"] = true,
+    ["Toucan"] = true,
+    ["Wasp"] = true,
+    ["Honey Bee"] = true
   },
-  Legendary = {
-    "Cow", "Silver Monkey", "Tarantula Hawk", "Mole",
-    "Seal", "Orangutan", "Peacock", "Ostrich",
-    "Axolotl", "Triceratops", "Stegosaurus", "Scarlet Macaw",
-    "Polar Bear", "Sea Otter", "Turtle"
+  ["Legendary"] = {
+    ["Cow"] = true,
+    ["Silver Monkey"] = true,
+    ["Tarantula Hawk"] = true,
+    ["Mole"] = true,
+    ["Seal"] = true,
+    ["Orangutan"] = true,
+    ["Peacock"] = true,
+    ["Ostrich"] = true,
+    ["Axolotl"] = true,
+    ["Triceratops"] = true,
+    ["Stegosaurus"] = true,
+    ["Scarlet Macaw"] = true,
+    ["Polar Bear"] = true,
+    ["Sea Otter"] = true,
+    ["Turtle"] = true
   },
-  Mythical = {
-    "Snail", "Giant Ant", "Caterpillar", "Praying Mantis",
-    "Butterfly", "Echo Frog", "Frog", "Moth",
-    "Hyacinth Macaw", "Petal Bee", "Bear Bee"
+  ["Mythical"] = {
+    ["Snail"] = true,
+    ["Giant Ant"] = true,
+    ["Caterpillar"] = true,
+    ["Praying Mantis"] = true,
+    ["Butterfly"] = true,
+    ["Echo Frog"] = true,
+    ["Frog"] = true,
+    ["Moth"] = true,
+    ["Hyacinth Macaw"] = true,
+    ["Petal Bee"] = true,
+    ["Bear Bee"] = true
   },
-  Divine = {
-    "Dragonfly", "Queen Bee", "Raccoon", "Night Owl",
-    "Fennec Fox", "Disco Bee", "Mimic Octopus", "T‑Rex"
+  ["Divine"] = {
+    ["Dragonfly"] = true,
+    ["Queen Bee"] = true,
+    ["Raccoon"] = true,
+    ["Night Owl"] = true,
+    ["Fennec Fox"] = true,
+    ["Disco Bee"] = true,
+    ["Mimic Octopus"] = true,
+    ["T‑Rex"] = true
   }
 }
+
