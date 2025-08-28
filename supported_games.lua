25bb0021e05359991ba139b4f7f2cbe9185dc021
Easy lua <staskuzmin1406@gmail.com>
2025-08-28 15:49:51 +0300

    Update supported_games.lua

--- supported_games.lua
+++ supported_games.lua
@@ -6,7 +6,7 @@ local games_id = {
     ["105520955111026"] = "FREE UGC Obby Hair",
     ["6654918151"] = "Trench War",
     ["100024922559690"] = "Steal A Snack",
-    ["101523718981169"] = "Steal A Meme"
+    ["113909146512320"] = "Steal A Meme"
 }
 
 return games_id
