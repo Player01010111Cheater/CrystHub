d4959ce773ebfb24a6e53eee099e03bf74f36891
Easy lua <staskuzmin1406@gmail.com>
2025-08-29 21:31:14 +0300

    Update SafeCall.lua

--- utils/SafeCall.lua
+++ utils/SafeCall.lua
@@ -2,8 +2,10 @@ function SafeCall(func, ...)
     local success, result = pcall(func, ...)
 
     if not success then
-        warn("Finded Error In Script, wait for fix.")
+        warn("[SafeCall]: Finded Error In Script, wait for fix.")
         return nil
+    else
+        print("[SafeCall]: Script executed successfully!")
     end
 
     return result
