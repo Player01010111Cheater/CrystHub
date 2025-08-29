f43d356e4cba0dcf4ee6ce337291526902b83ef5
Easy lua <staskuzmin1406@gmail.com>
2025-08-29 19:54:42 +0300

    Update Init.lua

--- utils/Init.lua
+++ utils/Init.lua
@@ -1,6 +1,17 @@
 local function req(url)
-  
+    return loadstring(game:HttpGet(url))()
+end
 
 local utils = {
-  SafeCall = loadstring(game:)
+    SafeCall = function(...)
+        local f = req("https://raw.githubusercontent.com/Player01010111Cheater/CrystHub/refs/heads/main/utils/SafeCall.lua")
+        utils.SafeCall = f
+        return f(...)
+    end,
+
+    CustomCFrame = function(...)
+        local f = req("https://raw.githubusercontent.com/Player01010111Cheater/CrystHub/refs/heads/main/utils/CustomCFrame.lua")
+        utils.CustomCFrame = f
+        return f(...)
+    end,
 }
