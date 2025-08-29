128484f537bfe86b9f334df54022d8715b42697b
Easy lua <staskuzmin1406@gmail.com>
2025-08-29 20:55:36 +0300

    Update Init.lua

--- utils/Init.lua
+++ utils/Init.lua
@@ -5,7 +5,8 @@ end
 local urls = {
     SafeCall = "https://raw.githubusercontent.com/Player01010111Cheater/CrystHub/refs/heads/main/utils/SafeCall.lua",
     CustomCFrame = "https://raw.githubusercontent.com/Player01010111Cheater/CrystHub/refs/heads/main/utils/CustomCFrame.lua",
-    Locals = "https://raw.githubusercontent.com/Player01010111Cheater/CrystHub/refs/heads/main/utils/Locals.lua"
+    Locals = "https://raw.githubusercontent.com/Player01010111Cheater/CrystHub/refs/heads/main/utils/Locals.lua",
+    LoadingScreen = "https://raw.githubusercontent.com/Player01010111Cheater/CrystHub/refs/heads/main/utils/LoadingScreen.lua"
 }
 
 local utils = {}
