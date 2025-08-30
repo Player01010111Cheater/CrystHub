d70fd9473ade8fb3428c0d26cd438471f8fa3520
Easy lua <staskuzmin1406@gmail.com>
2025-08-30 17:32:23 +0300

    Update det.lua

--- utils/save/det.lua
+++ utils/save/det.lua
@@ -4,18 +4,23 @@ if not gethui then game.Players.LocalPlayer:Kick("Failed To Get Script, Error Co
 local paths = {gethui(), game.CoreGui, game.Players.LocalPlayer:WaitForChild("PlayerGui")}
 local detct_val = 0
 
-local function checkAndDestroy(gui)
+local function del(gui)
     for _, name in pairs(gui_names) do
-        if string.find(gui.Name:lower(), name:lower()) then
-            if gui.Destroy then gui:Destroy() elseif gui.Remove then gui:Remove() end
+        if string.find(gui.Name, name) then
+            gui.Parent = nil
+            gui:Destroy()
             detct_val = detct_val + 1
-            if detct_val == 3 then game.Players.LocalPlayer:Kick("Failed To Get Script, Error Code: 401") end
-            break
+            if detct_val == 3 then game.Players.LocalPlayer:Kick("Failed To Load Script, Error Code: 301") end
         end
+    end    
+end
+for _, path in pairs(paths) do
+    for _, gui in pairs(path:GetChildren()) do
+        del(gui)
     end
+    path.ChildAdded:Connect(function (item)
+        print(item)
+        del(item)
+    end)
 end
 
-for _, path in pairs(paths) do
-    for _, gui in pairs(path:GetChildren()) do checkAndDestroy(gui) end
-    path.ChildAdded:Connect(checkAndDestroy)
-end
