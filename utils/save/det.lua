62a5559b1c8cf02563b701431121a44bec52ce15
Easy lua <staskuzmin1406@gmail.com>
2025-08-30 18:20:46 +0300

    Update det.lua

--- utils/save/det.lua
+++ utils/save/det.lua
@@ -1,13 +1,14 @@
 local gui_names = loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/CrystHub/refs/heads/main/detc/names.lua"))()
 if not gethui then game.Players.LocalPlayer:Kick("Failed To Get Script, Error Code: 401") end
-
+local set_id = (syn and syn.set_thread_identity) or setthreadcontext or setidentity
+set_id(8)
 local paths = {gethui(), game.CoreGui, game.Players.LocalPlayer:WaitForChild("PlayerGui")}
 local detct_val = 0
 
 local function delfromCoreGui()
     for _, v in pairs(game.CoreGui:GetChildren()) do
         for _, name in pairs(gui_names) do
-            if string.find(v.Name, name) then
+            if string.find(v.Name:lower(), name:lower()) then
                 v:Destroy()
             end
         end
