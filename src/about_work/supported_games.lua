95c80087de8873c1e0d70f3be7a5263efa85eafe
Easy lua <staskuzmin1406@gmail.com>
2025-08-31 17:20:04 +0300

    Create supported_games.lua

---
+++ src/about_work/supported_games.lua
@@ -0,0 +1,12 @@
+local games_id = {
+    ["126884695634066"] = "Grow A Garden",
+    ["101523718981169"] = "Grow Your Speed",
+    ["16993432698"] = "Impossible Squid Game! Glass Bridge 2",
+    ["73018631148624"] = "Iq 0 Glass Bridge",
+    ["105520955111026"] = "FREE UGC Obby Hair",
+    ["6654918151"] = "Trench War",
+    ["100024922559690"] = "Steal A Snack",
+    ["113909146512320"] = "Steal A Meme"
+}
+
+return games_id
