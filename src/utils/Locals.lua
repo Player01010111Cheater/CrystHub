5b077d211e1f49202e5c8543b2bfa03c4b68093b
Easy lua <staskuzmin1406@gmail.com>
2025-08-31 17:29:01 +0300

    Update Locals.lua

--- src/utils/Locals.lua
+++ src/utils/Locals.lua
@@ -1,1 +1,8 @@
-
+return function()
+    local LocalVars = {}
+    LocalVars.players = game:GetService("Players")
+    LocalVars.player = LocalVars.players.LocalPlayer
+    LocalVars.replicated_st = game:GetService("ReplicatedStorage")
+    LocalVars.playergui = LocalVars.player.PlayerGui
+    return LocalVars
+end
