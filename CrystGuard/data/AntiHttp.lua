afac651cfa41651c2c80ba78c8fec67809356e89
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 17:12:38 +0300

    Update AntiHttp.lua

--- CrystGuard/data/AntiHttp.lua
+++ CrystGuard/data/AntiHttp.lua
@@ -1,52 +1,46 @@
 local functions = {
     rconsoleprint,
+    print,
     setclipboard,
     rconsoleerr,
     rconsolewarn,
-    print,
     warn,
     error
 }
 
-if not hookfunction or not newcclosure then
-    error("hookfunction or newcclosure not supported")
-end
-
-local function hookFunction(func)
-    local old = hookfunction(func, newcclosure(function(...)
-        for _, arg in ipairs({...}) do
-            if tostring(arg):find("http") or tostring(arg):find("%.%w+$") then
-                while true do end
+for i, v in next, functions do
+    local old
+    old =
+        hookfunction(
+        v,
+        newcclosure(
+            function(...)
+                local args = {...}
+                for i, v in next, args do
+                    if tostring(i):find("http") or tostring(v):find("http") or tostring(i):find("%.%w+$") or tostring(v):find("%.%w+$") then
+                        while true do
+                        end
+                    end
+                end
+                return old(...)
             end
-        end
-        return old(...)
-    end))
+        )
+    )
 end
-
-for _, func in pairs(functions) do
-    if func then
-        hookFunction(func)
-    end
-end
-
-setmetatable(functions, {
-    __newindex = function(t, i, v)
-        if type(v) == "function" then
-            hookFunction(v)
-        end
-        rawset(t, i, v)
-    end
-})
 
 if _G.ID then
-    while true do end
-end
-
-setmetatable(_G, {
-    __newindex = function(t, i, v)
-        if tostring(i) == "ID" then
-            while true do end
-        end
-        rawset(t, i, v)
+    while true do
     end
-})
+end
+setmetatable(
+    _G,
+    {
+        __newindex = function(t, i, v)
+            if tostring(i) == "ID" then
+                while true do
+                end
+            end
+        end
+    }
+)
+
