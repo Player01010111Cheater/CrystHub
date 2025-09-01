9020ec9ba48c1025185eb14d5883887212a2ae0a
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 19:45:13 +0300

    Create status.lua

---
+++ status.lua
@@ -0,0 +1,29 @@
+local status = {
+  "
+  🟢 = Working
+  🟡 = Down [ Fixing / Risk Ban ]
+  🟠 = Developing [ New Game / Updating ]
+  🔴 = Discontinued [ Doesnt Work / UnSupported ]
+
+  Working on [ PC ]
+    ∣| Grow A Garden : 🟠
+    ∣| Grow Your Speed : 🟢
+    ∣| Impossible Squid Game! Glass Bridge 2 : 🟢
+    ∣| Iq 0 Glass Bridge : 🟢
+    ∣| FREE UGC Obby Hair : 🟢
+    ∣| Trench War : 🟢
+    ∣| Steal A Snack : 🟢
+    ∣| Steal A Meme : 🟢
+
+  Working on [ Mobile ]
+    ∣| Grow A Garden : 🟠
+    ∣| Grow Your Speed : 🟢
+    ∣| Impossible Squid Game! Glass Bridge2 : 🟢
+    ∣| Iq0 Glass Bridge : 🟢
+    ∣| FREE UGC Obby Hair : 🟢
+    ∣| Trench War : 🟢
+    ∣| Steal A Snack : 🟢
+    ∣| Steal A Meme : 🟢
+  "
+}
+return status
