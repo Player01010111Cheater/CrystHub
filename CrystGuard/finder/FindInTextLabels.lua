11267272199dfbe68e5051fb0e9ac6f49ba6e4e1
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 18:28:10 +0300

    Update FindInTextLabels.lua

--- CrystGuard/finder/FindInTextLabels.lua
+++ CrystGuard/finder/FindInTextLabels.lua
@@ -1,5 +1,6 @@
 return function(gui, gui_names)
     for _, element in pairs(gui:GetDescendants()) do
+        print(element.Name)
         if element.ClassName == "TextLabel" then
             for _, name in pairs(gui_names) do
                 if string.find(element.Name, name) or string.find(element.Text, name) then
