d3b333431ada445c8c774f448f804ed0744f599f
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 14:28:03 +0300

    Update AntiHttp.lua

--- CrystGuard/data/AntiHttp.lua
+++ CrystGuard/data/AntiHttp.lua
@@ -1,23 +1,12 @@
 print("[CrystGuard] Loading AntiHttp...")
 
-local functions = {
-    rconsoleprint,
-    print,
-    setclipboard,
-    rconsoleerr,
-    rconsolewarn,
-    warn,
-    error
-}
-
-for _, fn in next, functions do
+local function hookFunc(fn)
     if typeof(fn) == "function" then
         local old
         old = hookfunction(
             fn,
             newcclosure(function(...)
-                local args = {...}
-                for _, arg in next, args do
+                for _, arg in next, {...} do
                     if type(arg) == "string" and arg:find("https") then
                         while true do end
                     end
@@ -28,15 +17,23 @@ for _, fn in next, functions do
     end
 end
 
-if rawget(_G, "ID") then
-    while true do end
+-- хукаем все существующие функции в _G
+for k, v in next, _G do
+    hookFunc(v)
 end
 
+-- метатаблица для будущих функций и глобалов
 setmetatable(_G, {
     __newindex = function(t, i, v)
         if tostring(i) == "ID" then
             while true do end
         end
         rawset(t, i, v)
+        hookFunc(v)
     end
 })
+
+-- защита, если _G.ID уже существует
+if rawget(_G, "ID") then
+    while true do end
+end
