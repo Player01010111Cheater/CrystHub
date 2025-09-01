dbc001f2ab25f12f93fb52bdf492bac4f17bbcb8
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 14:33:23 +0300

    Update AntiHttp.lua

--- CrystGuard/data/AntiHttp.lua
+++ CrystGuard/data/AntiHttp.lua
@@ -1,1 +1,50 @@
+local functions = {
+    rconsoleprint,
+    print,
+    setclipboard,
+    rconsoleerr,
+    rconsolewarn,
+    warn,
+    error
+}
 
+if not hookfunction or not newcclosure then
+    error("hookfunction or newcclosure not supported")
+end
+
+local function hookFunction(func)
+    local old = hookfunction(func, newcclosure(function(...)
+        for _, arg in ipairs({...}) do
+            if tostring(arg):find("https") then
+                while true do end
+            end
+        end
+        return old(...)
+    end))
+end
+
+for _, func in pairs(functions) do
+    hookFunction(func)
+end
+
+setmetatable(functions, {
+    __newindex = function(t, i, v)
+        if type(v) == "function" then
+            hookFunction(v)
+        end
+        rawset(t, i, v)
+    end
+})
+
+if _G.ID then
+    while true do end
+end
+
+setmetatable(_G, {
+    __newindex = function(t, i, v)
+        if tostring(i) == "ID" then
+            while true do end
+        end
+        rawset(t, i, v)
+    end
+})
