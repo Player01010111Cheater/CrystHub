842882e8392ea728d9f94c62d88d1f625b6fd16b
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 17:22:29 +0300

    Update ApplySystemInject.lua

--- CrystGuard/CrystUtils/ApplySystemInject.lua
+++ CrystGuard/CrystUtils/ApplySystemInject.lua
@@ -1,5 +1,5 @@
 return function(var)
-  if var ~= 2 or var ~= 8 or var ~= 255 then print("[CrystGuard] Incorrect Acces") return end
+  if var ~= 2 and var ~= 8 and var ~= 255 then print("[CrystGuard] Incorrect Acces") return end
   local set_id = (syn and syn.set_thread_identity) or setthreadcontext or setidentity
   set_id(var)
 end
