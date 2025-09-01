c3f9444e0ca67a4cefbfd9c57143a2a8603f0571
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 12:19:08 +0300

    Delete CrystGuard/fun directory

--- CrystGuard/fun/PowerConfig.lua
+++
@@ -1,7 +0,0 @@
-local PowerConfig = {
-  Range = 3, -- 1 / 10,
-  Power = 50, -- 1 / 100 %
-  SystemInject = 1, -- 1 / 3 
-  Performance = 1, -- 1 / 10
-  Webhook = false, -- true / false
-}
--- CrystGuard/fun/PowerUtil/ApplySystemInject.lua
+++
@@ -1,5 +0,0 @@
-return function(var)
-  if var ~= 2 or var ~= 8 or var ~= 255 then print("[CrystGuard] Incorrect Acces") return end
-  local set_id = (syn and syn.set_thread_identity) or setthreadcontext or setidentity
-  set_id(var)
-end
