a4dfb0216a914378e82270fa4f14d55004342980
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 12:19:51 +0300

    Update Manager.lua

--- CrystGuard/CrystUtils/Manager.lua
+++ CrystGuard/CrystUtils/Manager.lua
@@ -1,1 +1,25 @@
+local function req(url)
+    return loadstring(game:HttpGet(url))()
+end
+
+local cryst_items = {
+    ApplySystemInject = "",
+    SendWebHook = ""
+}
+
+local manager = {}
+
+for name, item in pairs(cryst_items) do
+    if type(item) == "string" and item:match("^https?://") then
+        manager[name] = function(...)
+            local f = req(item)
+            manager[name] = f
+            return f(...)
+        end
+    else
+        manager[name] = item
+    end
+end
+
+return manager
 
