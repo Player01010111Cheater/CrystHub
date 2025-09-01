e538a6bf4e00e442ee344c5371ad149736a5f095
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 12:17:52 +0300

    Delete src/utils directory

--- src/utils/Locals.lua
+++
@@ -1,8 +0,0 @@
-return function()
-    local LocalVars = {}
-    LocalVars.players = game:GetService("Players")
-    LocalVars.player = LocalVars.players.LocalPlayer
-    LocalVars.replicated_st = game:GetService("ReplicatedStorage")
-    LocalVars.playergui = LocalVars.player.PlayerGui
-    return LocalVars
-end
