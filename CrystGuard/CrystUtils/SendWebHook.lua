86afb7804704d8bbf06357e5a5e3b4d912dc294a
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 12:20:43 +0300

    Update SendWebHook.lua

--- CrystGuard/CrystUtils/SendWebHook.lua
+++ CrystGuard/CrystUtils/SendWebHook.lua
@@ -1,1 +1,16 @@
-
+return function(url, text)
+    local https = game:GetService("HttpService")
+    local data = {
+        ["content"] = text,
+        ["username"] = "CrystGuard-Notification"
+    }
+    local body = https:JSONEncode(data)
+    request({
+        Url = url,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = body
+    })
+end
