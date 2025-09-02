7e8a7a65215c7f4ccd45c34c1896926c37da9ed8
Easy lua <staskuzmin1406@gmail.com>
2025-09-02 14:48:22 +0300

    Update SendWebHook.lua

--- CrystGuard/CrystUtils/SendWebHook.lua
+++ CrystGuard/CrystUtils/SendWebHook.lua
@@ -2,18 +2,20 @@ local https = game:GetService("HttpService")
 local player = game.Players.LocalPlayer
 return function(url, detected, detect_val)
     local cont = {
+        "➖➖➖➖➖➖➖➖➖",
         "👤 Username: " .. player.Name,
         "🔎 DisplayName: " .. player.DisplayName,
         "🆔 UserId: " .. player.UserId,
         "➖➖➖➖➖➖➖➖➖",
-        "⚠ Detected: " .. detected,
+        "⚠ Detect Name: " .. detected,
         "🔰 Detect Values: " .. detect_val,
+        "➖➖➖➖➖➖➖➖➖"
 
     }
     local text = table.concat(cont, "\n")
     local data = {
         ["content"] = text,
-        ["username"] = "CrystGuard-Notification"
+        ["username"] = "CrystGuard"
     }
     local body = https:JSONEncode(data)
     request({
